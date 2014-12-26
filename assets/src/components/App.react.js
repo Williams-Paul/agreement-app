ade2e3d4910cd7583e5e760d76a7f0dc9f9c8d66
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-26 01:26:22 -0500

    Updated app react component to show product form.

--- assets/src/components/App.react.js
+++ assets/src/components/App.react.js
@@ -1,11 +1,25 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var React = require('react'),
+  Bootstrap = require('react-bootstrap'),
+  Grid = React.createFactory(Bootstrap.Grid),
+  Row = React.createFactory(Bootstrap.Row),
+  Col = React.createFactory(Bootstrap.Col),
+  PageHeader = React.createFactory(Bootstrap.PageHeader);
+
+var ProductForm = React.createFactory(require('./product/ProductForm.react'));
 
 var App = React.createClass({
   render: function() {
    return (
-     <div>Agreement App</div>
+     <Grid>
+      <Row>
+        <Col lg={12} xs={12}>
+          <PageHeader>Formulario</PageHeader>
+          <ProductForm/>
+        </Col>
+      </Row>
+     </Grid>
    ); 
   }
 });
