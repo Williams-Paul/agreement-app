6531e81506d91aaa60f331796e5b67aaecb29531
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-30 21:01:19 -0500

    Added Unit Model and Controller

---
+++ api/controllers/UnitController.js
@@ -0,0 +1,16 @@
+/**
+ * UnitController
+ *
+ * @description :: Server-side logic for managing units
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	name: {
+    type: 'string'
+  },
+  title: {
+    type: 'string'
+  }
+};
+
