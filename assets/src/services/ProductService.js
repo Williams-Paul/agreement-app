40b0a6db901be9e89c4fa696ba970e0897201302
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-31 10:04:43 -0500

    Added Product Service.

---
+++ assets/src/services/ProductService.js
@@ -0,0 +1,46 @@
+/**
+ * ProductService
+ */
+
+var request = require('superagent');
+require('q-superagent')(request);
+
+var _url = '/product/';
+
+ProductService = {
+  create: function(data, success, failure) {
+    request.post(_url).send(data).q(function(res) {
+      var data = res.body;
+      success(data);
+    }).fail(function(err) {
+      failure(err);
+    });
+  },
+  
+  update: function() {
+    
+  },
+  
+  completeAll: function() {
+    
+  },
+  
+  destroy: function() {
+    
+  },
+  
+  destroyCompleted: function() {
+    
+  },
+  
+  load: function(success, failure) {
+    request.get(_url).q(function(res) {
+      var data = res.body;
+      success(data);
+    }).fail(function(error) {
+      failure(error);
+    });
+  }
+}
+
+module.exports = ProductService;
