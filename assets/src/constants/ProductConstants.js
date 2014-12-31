e4977a68b05dfae93bdbe648773fd8f67cb81432
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-31 10:04:27 -0500

    Added Product Constants.

--- assets/src/constants/ProductConstants.js
+++ assets/src/constants/ProductConstants.js
@@ -5,6 +5,11 @@
 var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
-  'PRODUCT_CREATE'
+  'PRODUCT_CREATE': null,
+  'PRODUCT_CREATE_SUCCESS': null,
+  'PRODUCT_CREATE_FAIL': null,
+  
+  'PRODUCT_LOAD': null,
+  'PRODUCT_LOAD_SUCCESS': null,
+  'PRODUCT_LOAD_FAIL': null
 });
-
