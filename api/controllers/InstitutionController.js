1920d3da1072c2b5759a84a72aceac09b6465cab
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-31 15:53:48 -0500

    Created Institution model and controller.

---
+++ api/controllers/InstitutionController.js
@@ -0,0 +1,11 @@
+/**
+ * InstitutionController
+ *
+ * @description :: Server-side logic for managing institutions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
