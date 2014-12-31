451feba3c066b95dd2dedeaa1e97487323a925be
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-31 10:04:10 -0500

    Added Product Actions.

--- assets/src/actions/ProductActions.js
+++ assets/src/actions/ProductActions.js
@@ -4,6 +4,7 @@
 
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var ProductConstants = require('../constants/ProductConstants');
+var ProductService = require('../services/ProductService');
 
 var ProductActions = {
 
@@ -11,10 +12,41 @@ var ProductActions = {
    * @param  {string} text
    */
   create: function(params) {
+    
     AppDispatcher.handleViewAction({
-      actionType: ProductConstants.PRODUCT_CREATE,
-      params: params
+      actionType: ProductConstants.PRODUCT_CREATE
     });
+    
+    ProductService.create(params, function(data) {
+      AppDispatcher.handleViewAction({
+        actionType: ProductConstants.PRODUCT_CREATE_SUCCESS,
+        data: data
+      });
+    }, function(error) {
+      AppDispatcher.handleViewAction({
+        actionType: ProductConstants.PRODUCT_CREATE_FAILURE,
+        error: error
+      });
+    });
+  },
+  load: function() {
+    
+    AppDispatcher.handleViewAction({
+      actionType: ProductConstants.PRODUCT_LOAD
+    });
+    
+    ProductService.load(function(data) {
+      AppDispatcher.handleViewAction({
+        actionType: ProductConstants.PRODUCT_LOAD_SUCCESS,
+        data: data
+      });
+    }, function(error) {
+      AppDispatcher.handleViewAction({
+        actionType: ProductConstants.PRODUCT_LOAD_FAIL,
+        error: error
+      });
+    });
+    
   }
 };
 
