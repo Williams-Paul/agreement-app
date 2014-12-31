645a46ff435b4090d84480fcb231a2fea3d9c577
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-31 15:54:20 -0500

    Created purpose model and controller.

---
+++ api/controllers/PurposeController.js
@@ -0,0 +1,11 @@
+/**
+ * PurposeController
+ *
+ * @description :: Server-side logic for managing purposes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
