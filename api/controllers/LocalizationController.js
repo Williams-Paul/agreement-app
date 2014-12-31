03ec685740e6063d9b26e03e3e15e958abd79645
Joseph Ramos <joseph.ramos@taller.net.br>
2014-12-31 15:54:51 -0500

    Created localization model and controller.

---
+++ api/controllers/LocalizationController.js
@@ -0,0 +1,11 @@
+/**
+ * LocalizationController
+ *
+ * @description :: Server-side logic for managing localizations
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
