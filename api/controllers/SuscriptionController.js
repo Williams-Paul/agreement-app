da04c065e3be5e9de5ed5b77115134616afd5158
Joseph Ramos <joseph.ramos@taller.net.br>
2015-01-02 11:31:58 -0500

    Added REST api to suscriptions

---
+++ api/controllers/SuscriptionController.js
@@ -0,0 +1,11 @@
+/**
+ * SuscriptionController
+ *
+ * @description :: Server-side logic for managing suscriptions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
