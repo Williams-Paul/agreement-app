a0bb8b3f5d1aca9b889885851c43999204befa47
Joseph Ramos <joseph.ramos@taller.net.br>
2015-01-02 17:40:48 -0500

    Delete institution service implemented

--- assets/src/constants/InstitutionConstants.js
+++ assets/src/constants/InstitutionConstants.js
@@ -1,6 +1,6 @@
 /**
- * ProductConstant
- */ 
+ * ProductConstants
+ */
 
 var keyMirror = require('keymirror');
 
@@ -8,7 +8,11 @@ module.exports = keyMirror({
   'INSTITUTION_CREATE': null,
   'INSTITUTION_CREATE_SUCCESS': null,
   'INSTITUTION_CREATE_FAIL': null,
-  
+
+  'INSTITUTION_DELETE': null,
+  'INSTITUTION_DELETE_SUCCESS': null,
+  'INSTITUTION_DELETE_FAILURE': null,
+
   'INSTITUTION_LOAD': null,
   'INSTITUTION_LOAD_SUCCESS': null,
   'INSTITUTION_LOAD_FAIL': null
